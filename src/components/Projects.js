3cb89611e9f869ff660ec34ea8c03485a8ae1090
hanzaa <farhan.2523@gmail.com>
2022-12-12 13:39:00 +0700

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -21,11 +21,11 @@ const Projects = () => {
                 </div>
                 <div class="col-md-6 col-xl-4 d-flex justify-content-center my-4">
                     <div class="card" >
-                        <a href='https://www.google.com/' target='_blank' rel='noopener noreferrer'>
-                        <img src={require("../assets/open-book.png")} class="card-img-top" alt="Prjoect Preview" width="100%" height="100%"/>    
+                        <a href='https://luang.vercel.app/' target='_blank' rel='noopener noreferrer'>
+                        <img src={require("../assets/luang.png")} class="card-img-top" alt="Prjoect Preview" width="100%" height="100%"/>    
                         </a>
                         <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <p class="card-text">Website marketplace untuk layanan freelancer untuk membantu para pekerja lepas memasarkan jasa yang mereka miliki dengan menghubungkan antara klien dengan para freelancer.</p>
                         </div>
                     </div>
                 </div>
