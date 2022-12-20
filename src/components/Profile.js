daf30f0a8118d0943998cda3589f4331116cf736
hanzaa <farhan.2523@gmail.com>
2022-12-20 09:06:34 +0700

    Update Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -12,7 +12,7 @@ const Profile = () => {
                                                         <div class="col-sm-4 bg-c-lite-green user-profile">
                                                             <div class="card-block text-center text-white">
                                                                 <div class="m-b-25">
-                                                                    <img src={pp} class="img-radius rounded-circle" width="80%" alt="User-Profile-Image"/>
+                                                                    <img src={pp} class="img-radius rounded-circle" width="80%" alt="User-Profile"/>
                                                                 </div>
                                                                 <h6 class="f-w-600">M Farhan Zachary</h6>
                                                                 <p>Web Designer</p>
@@ -48,11 +48,6 @@ const Profile = () => {
                                                                         <p class="m-b-10 f-w-600">Do Your Best, Let God Do The Rest</p>
                                                                     </div>
                                                                 </div>
-                                                                <ul class="social-link list-unstyled m-t-40 m-b-10">
-                                                                    <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="facebook" data-abc="true"><i class="mdi mdi-facebook feather icon-facebook facebook" aria-hidden="true"></i></a></li>
-                                                                    <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="twitter" data-abc="true"><i class="mdi mdi-twitter feather icon-twitter twitter" aria-hidden="true"></i></a></li>
-                                                                    <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="instagram" data-abc="true"><i class="mdi mdi-instagram feather icon-instagram instagram" aria-hidden="true"></i></a></li>
-                                                                </ul>
                                                             </div>
                                                         </div>
                                                     </div>
